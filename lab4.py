d77fde6d5c2d2accaa67cff9c5e1ba6438c51dab
powei94 <cristobal.becerraf@usach.cl>
2017-11-12 02:51:41 -0300

    moduladores listos

--- lab4.py
+++ lab4.py
@@ -2,24 +2,18 @@ import scipy.io.wavfile as wav
 import matplotlib.pyplot as matplot
 import numpy as np
 import math
-from scipy import fftpack
+from scipy import fftpack, integrate
 import warnings
 
-def plotDataTime(d, f):     # Función para graficar en el tiempo
-    n = len(d)
-    T = n/f   # duración del audio
-    t = np.arange(0,T, 1.0/f)
-    matplot.plot(t, d)
-    matplot.xlabel('Tiempo (s)')
-    matplot.ylabel('Amplitud')
-    matplot.show()
 
 def generateCarrier(d, f):
     n = np.size(d)
     T = n/f     #Tiempo
-    fc = 4*f    #Frecuencia de la portadora (4 veces la frecuencia de la señal)
+    fc = 4000000    #Frecuencia de la portadora (4 veces la frecuencia de la señal)
     tc = np.arange(0, T, 1.0/(fc))
     carrier = np.cos(2*f*np.pi*tc)
+    matplot.plot(carrier)
+    matplot.show()
     return (carrier, tc, fc)
 
 def AMmod(signal, f, M):
@@ -33,9 +27,27 @@ def AMmod(signal, f, M):
     tnew = np.arange(0, T, 1.0/(fc))
     signalInterp = np.interp(tnew, told, signal)   #Se interpola la señal original
     datamod = (1 + signalInterp)*carrier
-    signalInterp = signalInterp + Amax  #Shif Up para mantener señales sobre el eje y
+    signalInterp = signalInterp + Amax  #Shift Up para mantener señales sobre el eje y
     datamod = datamod + Amax
-    return (datamod, tnew, signalInterp)
+    return (datamod, tnew, signalInterp, carrier)
+
+def FMmod(signal, f, M):
+    M = M/100
+    n = np.size(signal)
+    T = n/f
+    fc = 4000000
+    tc = np.arange(0, T, 1.0/(fc))
+    carrier = np.cos(2*f*np.pi*tc)
+    told = np.arange(0, T, 1.0/f)
+    tnew = np.arange(0, T, 1.0/(fc))
+    t = np.linspace(0, n, np.size(tc))
+    print(np.size(t))
+    print(np.size(tc))
+    signalInterp = np.interp(tnew, told, signal)
+    integral = integrate.cumtrapz(signalInterp, tc, initial=0)
+    k = 1.0
+    fmMod = np.cos(2*np.pi*f*tc + k*integral)
+    return (fmMod, tnew, signalInterp, carrier)
 
 
 
@@ -44,36 +56,19 @@ if __name__ == '__main__':
     
 	# Se leen los archivos de audio, se guarda la frecuencia en f y las muestras en data
     f, signal = wav.read('handel.wav')
-    signal = signal[4000:4050]
+    signal = signal[5000:5050]
     
-    #carrier, tc, fc = generateCarrier(signal, f)
-    #signal_mod, td, signal_interp = AMmod(signal, f, 150)
 
-    #FM MOD
-    modulator_frequency = 4.0
-    fc = 40000
-    modulation_index = 1.0
+    fm, tnew, signalInterp, carrier = AMmod(signal, f, 100)
+    
 
-    n = len(signal)
-    T = n/f   # duración del audio
-    t = np.arange(0,T, 1.0/f)
-
-    modulator = signal
-    carrier = np.sin(2.0*np.pi*fc*t)
-    product = np.zeros_like(modulator)
-
-    for i,t in enumerate(t):
-        product[i] = np.sin(2.0*np.pi*(fc*t+modulator[i]))
-
-#    product = fftpack.fft(product)
     fp, axarr = matplot.subplots(3)
-    axarr[0].plot(modulator)
+    axarr[0].plot(signalInterp)
     axarr[0].set_title('Señal')
     axarr[1].plot(carrier)
-    axarr[1].set_title('Carrier')
-    axarr[2].plot(product)
-#    axarr[1].plot(time, modulator, color='r')
-    axarr[2].set_title('Señal modulada')
+    axarr[1].set_title('carrier')
+    axarr[2].plot(fm)
+    axarr[2].set_title('fm')
 
     matplot.show()
 
@@ -86,9 +81,3 @@ if __name__ == '__main__':
 
 
 
-
-#    plotDataTime(data, f)
-
-#    dmod = carrier*data
-
-#    plotDataTime(dmod, f)
